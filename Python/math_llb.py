f2a9a46af260413c9ac06508ff9e0a8acd67cadc
AnilSapra <sapraanil03@gmail.com>
2026-01-27 23:05:03 -0500

    practice of math library

---
+++ Python/math_llb.py
@@ -0,0 +1,23 @@
+import math
+
+no=100
+#sqrt
+
+print(f"square root of {no} is {math.sqrt(no)}")
+#power
+print("3 raised to 4",math.pow(3,4))
+#ceil,floor
+print("ceil value of 7.1",math.ceil(7.1))
+print("floor value of 7.8",math.floor(7.8))
+
+#pi
+print("pi value",math.pi)
+
+#factorial
+print("factorial of 5",math.factorial(5))
+
+#log
+print("log 100 base 10",math.log(100,10))
+
+#sin
+print("sin 90 value",math.sin(math.radians(90)))
